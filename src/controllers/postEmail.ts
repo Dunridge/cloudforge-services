463ab18679a2be90ca1c0d4e97be49cf150453a7
Dunridge <Polinka7max@gmail.com>
2024-05-21 08:06:06 -0700

    emails-input. add insert

--- src/controllers/postEmail.ts
+++ src/controllers/postEmail.ts
@@ -5,14 +5,14 @@ export const postEmail = async (req: Request, res: Response) => {
     // const requestBody = { email, subject, content }; // this is sent to the endpoint
     
     try {
-        const email = req.body;
+        const { subject, email, content } = req.body;
         // res.json({ message: 'endpoint reached', body: email });
     
         if (!collections.emails) {
             return res.status(500).json({ message: 'Database collection not initialized.' });
         }
     
-        const result = await collections.emails.insertOne({ email });
+        const result = await collections.emails.insertOne({ subject, email, content });
         return res.status(201).json({ message: 'Email inserted successfully', result });
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error' });
